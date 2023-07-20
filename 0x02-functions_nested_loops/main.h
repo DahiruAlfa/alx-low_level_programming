714f963a94ed35a46c26e4448a9d0f698544f161
DahiruAlfa <dahirualfa@gmail.com>
2023-07-20 13:17:43 -0700

    Alphabets printing

---
+++ 0x02-functions_nested_loops/main.h
@@ -0,0 +1,16 @@
+#ifndef _H_INCLUDE
+#define _H_INCLUDE
+int _putchar (char a);
+void print_alphabet(void);
+void print_alphabet_x10(void);
+int _islower(int a);
+int _isalpha(int a);
+int print_sign(int n);
+int _abs(int a);
+int print_last_digit(int);
+void jack_bauer(void);
+void times_table(void);
+int add(int, int);
+void print_to_98(int a);
+void print_times_table(int a);
+#endif
