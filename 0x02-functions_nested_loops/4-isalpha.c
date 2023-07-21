5b7f84354f7b21e52d8016cd33709bf4b9d685a4
DahiruAlfa <dahirualfa@gmail.com>
2023-07-21 08:58:02 -0700

    is Alphabet

--- 0x02-functions_nested_loops/4-isalpha.c
+++ 0x02-functions_nested_loops/4-isalpha.c
@@ -1,27 +1,17 @@
 #include "main.h"
+
 /**
- * print_sign - function to check for a sign of a number
- *
- * @n: is the int that will use for the argument of the function
- *
- * Return: 0
- *
+ * _isalpha - function to check if c is  a letter, lowercase or uppercase
+ * @a: is the int that will use for the argument of the function
+ * Return: 0 (Success)
  */
-int print_sign(int n)
+
+int _isalpha(int a)
 {
-	if (n > 0)
+	if ((a >= 'a' && a <= 'z') || (a >= 'A' && a <= 'Z'))
 	{
-		_putchar('+');
-		return (1);
-	}
-	else if (n < 0)
-	{
-		_putchar('-');
-		return (-1);
+	return (1);
 	}
 	else
-	{
-		_putchar('0');
-		return (0);
-	}
+	return (0);
 }
